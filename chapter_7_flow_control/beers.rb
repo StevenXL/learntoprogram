4a3de1d5f2cf37547548966f1f0a199404f93dcf
StevenXL <leiva.steven@gmail.com>
2014-12-31 11:53:29 -0500

    First problem of Chapter 7 complete

--- chapter_7_flow_control/beers.rb
+++ chapter_7_flow_control/beers.rb
@@ -1,12 +1,26 @@
-beers = 99
+initial_beers = 99
 
+while (initial_beers > 0)
+    # seperate song into lines to toggle between bottle and bottles later in
+    # the program
+    line_1_and_2 = "\t#{initial_beers} bottles of beer on the wall,\n\t#{initial_beers} bottles of beer!\n"
+    line_3 = "\tYou take one down, pass it around,\n"
+    line_4 = "\t#{initial_beers - 1} bottles of beer on the wall!\n"
 
-while (beers > 0)
-    song = "\t#{beers} bottles of beer on the wall,
-    \t#{beers} bottles of beer!
-    \tYou take one down, pass it around,
-    \t#{beers - 1} bottles of beer on the wall!\n"
+    # toggle between bottles and bottle
+    line_1_and_2.gsub!("bottles", "bottle") if (initial_beers < 2) 
+    line_4.gsub!("bottles", "bottle") if ( (initial_beers - 1) < 2) 
 
-    print song
-    beers = beers - 1
+    # corner case if there are 0 bottles in line 4
+    if ( (initial_beers - 1) == 0)
+        line_4 = "\t........... No more beer!!!\n"
+    end
+
+    print line_1_and_2
+    print line_3
+    print line_4
+
+    print "\n\n"
+
+    initial_beers = initial_beers - 1
 end
