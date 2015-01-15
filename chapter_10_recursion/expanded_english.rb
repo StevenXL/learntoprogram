8bf9be43c651b2f0fa57c4fd25a715afdb1889db
StevenXL <leiva.steven@gmail.com>
2015-01-15 10:41:48 -0500

    working expanded_english file in chapter 10

---
+++ chapter_10_recursion/expanded_english.rb
@@ -0,0 +1,74 @@
+def english_number(number)
+    if number < 0            # no negative numbers
+        return "Please enter a number that isn't negative"
+    end
+
+    if number == 0
+        return "zero"
+    end
+
+    num_string = ""         # this is the string we will be returning
+    
+    ones_place = [ "one" , "two" , "three",
+                   "four", "five", "six",
+                   "seven", "eight", "nine" ]
+
+    tens_place = [ "ten", "twenty", "thirty",
+                   "fourty", "fifty", "sixty",
+                   "seventy", "eighty", "ninety" ]
+
+    teenagers = [ "eleven", "twelve", "thirteen",
+                  "fourteen", "fifteen", "sixteen",
+                  "seventeen", "eighteen", "nineteen" ]
+    
+    # left is how much of the number we still have to write out
+    # "write" is the part we are writing out right now
+
+    left = number
+    write = left / 100            # how many hundreds left?
+    left = left - write * 100     # subtract off those hundreds
+
+    if write > 0
+        # here is the recursion
+        hundreds = english_number(write)
+
+        num_string = num_string + hundreds + "hundred"
+    end
+
+    if left > 0 
+        # if string is empty, do not add unnecessary spaces
+        if num_string.length > 0
+            num_string = num_string + " "
+        end
+    end
+
+    write = left / 10
+    left = left - write * 10
+
+    if write > 0
+        if ((write == 1) and (left > 0))
+            num_string = num_string + teenagers[left - 1]
+
+            left = 0
+        else
+            num_string = num_string + tens_place[write - 1]
+        end
+    end
+
+    if left > 0
+        # if string is empty, do not add unnecessary spaces
+        if num_string.length > 0
+            num_string = num_string + "-"
+        end
+    end
+
+    write = left
+
+    left = 0
+
+    if write > 0
+        num_string = num_string + ones_place[write - 1]
+    end
+
+    return num_string
+end
