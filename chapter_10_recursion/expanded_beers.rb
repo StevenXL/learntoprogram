ee97a2a69fc051dbad7a6fca475b7aa4d6950474
StevenXL <leiva.steven@gmail.com>
2015-01-15 10:56:30 -0500

    New beers script in chapter 10 works using expanded english script

---
+++ chapter_10_recursion/expanded_beers.rb
@@ -0,0 +1,28 @@
+require_relative "expanded_english"
+
+initial_beers = 9999
+
+while (initial_beers > 0)
+    # seperate song into lines to toggle between bottle and bottles later in
+    # the program
+    line_1_and_2 = "\t#{english_number(initial_beers)} bottles of beer on the wall,\n\t#{english_number(initial_beers)} bottles of beer!\n"
+    line_3 = "\tYou take one down, pass it around,\n"
+    line_4 = "\t#{english_number(initial_beers - 1)} bottles of beer on the wall!\n"
+
+    # toggle between bottles and bottle
+    line_1_and_2.gsub!("bottles", "bottle") if (initial_beers < 2) 
+    line_4.gsub!("bottles", "bottle") if ( (initial_beers - 1) < 2) 
+
+    # corner case if there are 0 bottles in line 4
+    if ( (initial_beers - 1) == 0)
+        line_4 = "\t........... No more beer!!!\n"
+    end
+
+    print line_1_and_2
+    print line_3
+    print line_4
+
+    print "\n\n"
+
+    initial_beers = initial_beers - 1
+end
--- chapter_10_recursion/test.rb
+++
@@ -1,3 +0,0 @@
-require_relative "expanded_english"
-
-puts english_number(1530564)
