3982754d9c1f2da390d0235e494ce25d7cd0083e
Quincy Iheme <qiheme@gmail.com>
2015-01-20 22:29:31 -0500

    code indentation and readme enhancement

--- chapter_02_numbers/authors_age.rb
+++ chapter_02_numbers/authors_age.rb
@@ -5,6 +5,8 @@ minutes_in_a_hour = 60
 hours_in_a_day = 24
 days_in_a_year = 365
 
+# multiplying seconds, minutes, hours and days to find out seconds in a year
 seconds_in_a_year = seconds_in_a_minute * minutes_in_a_hour * hours_in_a_day * days_in_a_year
 
+# print the result of your age in years
 puts sprintf("%.1f", age_in_years = age_in_seconds / seconds_in_a_year)
